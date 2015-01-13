a970171d86adae34c7b4aa73b80b1b57ba6d9590
joajida <tennogh@hotmail.fr>
2015-01-13 15:55:12 +0100

    Started using unittest in the functional tests, wrote a user story.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,35 @@
 from selenium import webdriver
-browser=webdriver.Chrome()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser=webdriver.Chrome()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#Let's visit this new website.
+		self.browser.get('http://localhost:8000')
+
+		#The page's title and header mention To-Do lists.
+		self.assertIn('To-Do',self.browser.title)
+		self.fail('Finish the test!')
+
+#We can enter an to-do item straight away.
+
+#We want to buy peacock feathers. Let's type it into a text box.
+
+#When we hit enter, the page updates and lists "peacock feathers" as a to-do item.
+
+#We can add another item. Let's add "Use feathers to make a fly".
+
+#The page updates again and now lists both items.
+
+#An explanatory text points out that the website has generated a unique URL for that to-do list. 
+
+#When we visit the URL, the to-do list is still there.
+
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
