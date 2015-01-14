1bd7a7c950c9b527ac55da2ee3c57caf39bbafee
joajida <tennogh@hotmail.fr>
2015-01-14 20:15:33 +0100

    Slightly refactored functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self,row_text):
+		table=self.browser.find_element_by_id('id_list_table')
+		rows=table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Let's visit this new website.
 		self.browser.get('http://localhost:8000')
@@ -29,9 +34,7 @@ class NewVisitorTest(unittest.TestCase):
 		#When we hit enter, the page updates and lists "peacock feathers" as a to-do item.
 		inputbox.send_keys(Keys.ENTER)
 
-		table=self.browser.find_element_by_id('id_list_table')
-		rows=table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+		self.check_for_row_in_list_table("1: Buy peacock feathers")
 
 		#We can add another item. Let's add "Use feathers to make a fly".
 		inputbox=self.browser.find_element_by_id('id_new_item')
@@ -39,10 +42,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		#The page updates again and now lists both items.
-		table=self.browser.find_element_by_id('id_list_table')
-		rows=table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-		self.assertIn('2: Use feathers to make a fly',[row.text for row in rows])
+		self.check_for_row_in_list_table("1: Buy peacock feathers")
+		self.check_for_row_in_list_table("2: Use feathers to make a fly")
 
 		#An explanatory text points out that the website has generated a unique URL for that to-do list. 
 		self.fail('Finish the test!')
