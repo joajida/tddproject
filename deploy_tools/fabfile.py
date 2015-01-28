fe3e5e8be0742632ec1d7ed5bb848e1317c9b5b6
joajida <tennogh@hotmail.fr>
2015-01-28 23:55:02 +0100

    fixed fabfiles not replacing DEBUG=True as intended

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -7,8 +7,8 @@ REPO_URL='https://github.com/joajida/tddproject.git'
 env.key_filename='~/.ssh/TDDto-doEC2instance.pem'
 
 def deploy():
-	site_folder='/home/%s/sites/%s' % (env.user, env.host)
-	source_folder=site_folder+'/source'
+	site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+	source_folder = site_folder + '/source'
 	_create_directory_structure_if_necessary(site_folder)
 	_get_latest_source(source_folder)
 	_update_settings(source_folder,env.host)
@@ -26,21 +26,22 @@ def _get_latest_source(source_folder):
 	else:
 		run('git clone %s %s' % (REPO_URL,source_folder))
 	current_commit=local("git log -n 1 --format=%H",capture=True)
-	run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+	run('cd %s && sudo git reset --hard %s' % (source_folder, current_commit))
 
 def _update_settings(source_folder,site_name):
 	settings_path=source_folder+'/superlists/settings.py'
-	sed(settings_path, "DEBUG=True", "DEBUG=False")
+	sed(settings_path, "DEBUG = True", "DEBUG=False",use_sudo=True)
 	sed(settings_path,
-		'ALLOWED_HOSTS=.+$',
-		'ALLOWED_HOSTS=["%s"]' % (site_name,)
+		'ALLOWED_HOSTS = .+$',
+		'ALLOWED_HOSTS=["%s"]' % (site_name,),
+		use_sudo=True
 		)
 	secret_key_file=source_folder+'/superlists/secret_key.py'
 	if not exists(secret_key_file):
 		chars='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 		key=''.join(random.SystemRandom().choice(chars) for _ in range(50))
 		append(secret_key_file,"SECRET_KEY='%s'" % (key,))
-	append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+	append(settings_path, '\nfrom .secret_key import SECRET_KEY',use_sudo=True)
 
 def _update_virtualenv(source_folder):
 	virtualenv_folder=source_folder+'/../virtualenv'
